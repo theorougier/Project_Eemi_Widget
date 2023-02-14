6061ee38f4f2adf72d97cd914f8b3dea19affe77
theorougier <theo_rougier@hotmail.fr>
2023-02-14 11:53:05 +0100

    'initialized'

--- react_widget_app/src/components/RestaurantDetails.js
+++ react_widget_app/src/components/RestaurantDetails.js
@@ -4,19 +4,10 @@ import photo from '../assets/img/Old_Jerusalem_Nafura_Restaurant_courtyard.jpg'
 
 export default function RestaurantDetails(restaurant) {
   const data = restaurant
-  const [image, setImage] = useState("");
-  useEffect(() => {
-    getImage();
-  }, []);
-  const getImage = async () => {
-      const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${restaurant.restaurant.photos[0].photo_reference}&key=AIzaSyCk2ucib5qc-uK1braaKGFQ2Bi2HNc4fws`
-      );
-  };
   return (
     <div key={data.restaurant.name} className="card-restaurant">
       <div><h2>{data.restaurant.name}</h2></div>
-      <div className="card-photo"><img src={image}/></div>
+      <div className="card-photo"><img src={photo}/></div>
       <div><span>{data.restaurant.vicinity}</span></div>
       <div className="card-informations">
         <div>note {data.restaurant.rating}/5</div>
